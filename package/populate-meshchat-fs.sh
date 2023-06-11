3156de73c013dee872e7163b1bec56c9bf4643c9
Gerard Hickey <hickey@kinetic-compute.com>
2023-06-11 14:10:48 -0400

    Added build MeshChat package job
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- package/populate-meshchat-fs.sh
+++ package/populate-meshchat-fs.sh
@@ -7,10 +7,14 @@ IPK_DIR=$1
 
 # Populate the CONTROL portion of the package
 mkdir -p $IPK_DIR/CONTROL
-cp -p package/meshchat-api/* $IPK_DIR/CONTROL/
+cp -p package/meshchat/* $IPK_DIR/CONTROL/
 sed -i "s%\$GITHUB_SERVER_URL%$GITHUB_SERVER_URL%" $IPK_DIR/CONTROL/control
 sed -i "s%\$GITHUB_REPOSITORY%$GITHUB_REPOSITORY%" $IPK_DIR/CONTROL/control
 
 # Populate the filesystem image for the package
-install -D meshchat $IPK_DIR/www/cgi-bin/meshchat
-
+install -d $IPK_DIR/www
+install www/* $IPK_DIR/www
+install -d $IPK_DIR/www/cgi-bin
+install cgi-bin/* $IPK_DIR/www/cgi-bin
+install -D support/init.d/meshchatsync $IPK_DIR/etc/init.d/meshchatsync
+install -D meshchatsync $IPK_DIR/usr/local/bin/meshchatsync
