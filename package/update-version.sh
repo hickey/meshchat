ce5cecfbf1fb6488ebf86063d434630b29dfe251
Gerard Hickey <hickey@kinetic-compute.com>
2023-06-11 12:39:51 -0400

    Added beginnings of package building code
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

---
+++ package/update-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script runs from the top of the project directory and
+# updates the version number in the control file for the package
+# being built.
+
+IPK_DIR=$1
+
+if [ "$GITHUB_REF_TYPE" == 'tag' ]; then
+    # ideally should only get version tags
+    if [ ${GITHUB_REF_NAME} : "v[0-9]" ]; then
+        version="${GITHUB_REF_NAME#v}"
+    fi
+else
+    # branch gets date code
+    version=$(date +%Y%m%d)
+fi
+
+sed -i "s/^Version:.*/Version: $version/" $IPK_DIR/CONTROL/control
