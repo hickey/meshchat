b616db4fb6f7a9f80b099f3e9060392a608d7459
Gerard Hickey <hickey@kinetic-compute.com>
2023-06-11 13:17:21 -0400

    Minor fixes to populate-meshchat-api-fs.sh
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- package/populate-meshchat-api-fs.sh
+++ package/populate-meshchat-api-fs.sh
@@ -7,8 +7,8 @@ IPK_DIR=$1
 
 mkdir -p $IPK_DIR/CONTROL
 cp -p package/meshchat-api/* $IPK_DIR/CONTROL/
-sed -i "s/\$GITHUB_SERVER_URL/$GITHUB_SERVER_URL/" \
-       "s/\$GITHUB_REPOSITORY/$GITHUB_REPOSITORY/" $IPK_DIR/CONTROL/control
+sed "s%\$GITHUB_SERVER_URL%$GITHUB_SERVER_URL%" $IPK_DIR/CONTROL/control
+sed "s%\$GITHUB_REPOSITORY%$GITHUB_REPOSITORY%" $IPK_DIR/CONTROL/control
 
-install -D meshchat $IPK_DIR/www/cgi-bin
+install -D meshchat $IPK_DIR/www/cgi-bin/meshchat
 
