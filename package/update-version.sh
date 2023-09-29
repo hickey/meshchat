3696b241ad3f8109544c1ef1bf3f3b6797e033de
Gerard Hickey <hickey@kinetic-compute.com>
2023-09-29 11:25:39 -0400

    build: Updates to better name packages
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- package/update-version.sh
+++ package/update-version.sh
@@ -6,18 +6,33 @@
 
 IPK_DIR=$1
 
-if [[ "$GITHUB_REF_TYPE" == 'tag' ]]; then
+if [[ "${GITHUB_REF_TYPE}" == 'tag' ]]; then
     # ideally should only get version tags (i.e. 'v' followed by a number)
     if [[ "${GITHUB_REF_NAME}" =~ ^v[0-9].* ]]; then
         version="${GITHUB_REF_NAME#v}"
+    fi
+elif [[ -n "${CI_COMMIT_TAG}" ]]; then
+    # ideally should only get version tags (i.e. 'v' followed by a number)
+    if [[ "${CI_COMMIT_TAG}" =~ ^v[0-9].* ]]; then
+        version="${CI_COMMIT_TAG#v}"
     fi
 else
     # branch gets date code-branch_name-commit
     date=$(date +%Y%m%d)
     branch=$(git rev-parse --abbrev-ref HEAD)
+    # maybe a detached head, so check common vars for branch name
+    if [[ -n "${CI_COMMIT_REF_NAME}" ]]; then
+        branch="${CI_COMMIT_REF_NAME}"
+    elif [[ -n "${GITHUB_REF_NAME}" ]]; then
+        branch="${GITHUB_REF_NAME}"
+    fi
     commit=$(git rev-parse --short HEAD)
     version="${date}-${branch}-${commit}"
 fi
+
+# write the version to a VERSION file
+echo "${version}" > VERSION
+echo "Updating code references to version ${version}"
 
 sed -i "s/^Version:.*/Version: $version/" $IPK_DIR/CONTROL/control
 
