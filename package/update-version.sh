74f5dee626ac74de28661382f2ae2fa7e38e6de9
Gerard Hickey <hickey@kinetic-compute.com>
2024-02-19 19:44:45 -0500

    add build job for branches

--- package/update-version.sh
+++ package/update-version.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 # This script runs from the top of the project directory and
@@ -45,25 +44,3 @@ sed -i "s/^Version:.*/Version: $version/" $IPK_DIR/CONTROL/control
 if [[ -f $IPK_DIR/www/cgi-bin/meshchatconfig.lua ]]; then
     sed -i "s/^app_version.*$/app_version                = \"${version}\"/" $IPK_DIR/www/cgi-bin/meshchatconfig.lua
 fi
-||||||| parent of 36f653c (Added beginnings of package building code)
-=======
-#!/bin/bash
-
-# This script runs from the top of the project directory and
-# updates the version number in the control file for the package
-# being built.
-
-IPK_DIR=$1
-
-if [ "$GITHUB_REF_TYPE" == 'tag' ]; then
-    # ideally should only get version tags
-    if [ ${GITHUB_REF_NAME} : "v[0-9]" ]; then
-        version="${GITHUB_REF_NAME#v}"
-    fi
-else
-    # branch gets date code
-    version=$(date +%Y%m%d)
-fi
-
-sed -i "s/^Version:.*/Version: $version/" $IPK_DIR/CONTROL/control
->>>>>>> 36f653c (Added beginnings of package building code)
