97613ed2641e1c23743e43b83bf7abe6734a271f
Gerard Hickey <hickey@kinetic-compute.com>
2024-04-06 18:31:39 -0400

    Replace luci.jsonc with pure Lua JSON library
    Signed-off-by: Gerard Hickey <hickey@kinetic-compute.com>

--- package/populate-meshchat-fs.sh
+++ package/populate-meshchat-fs.sh
@@ -4,6 +4,7 @@
 # creates the filesystem image for the MeshChat API package.
 
 IPK_DIR=$1
+OSNAME=$2
 
 # Populate the CONTROL portion of the package
 mkdir -p $IPK_DIR/CONTROL
@@ -12,11 +13,28 @@ sed -i "s%\$GITHUB_SERVER_URL%$GITHUB_SERVER_URL%" $IPK_DIR/CONTROL/control
 sed -i "s%\$GITHUB_REPOSITORY%$GITHUB_REPOSITORY%" $IPK_DIR/CONTROL/control
 
 # Populate the filesystem image for the package
-install -d $IPK_DIR/www/meshchat
-install www/* $IPK_DIR/www/meshchat
-install -d $IPK_DIR/www/cgi-bin
-install -m 755 meshchat $IPK_DIR/www/cgi-bin
-install -m 644 meshchatlib.lua $IPK_DIR/www/cgi-bin
-install -m 644 meshchatconfig.lua $IPK_DIR/www/cgi-bin
-install -D support/meshchatsync-init.d -m 755 $IPK_DIR/etc/init.d/meshchatsync
-install -D support/meshchatsync -m 755 $IPK_DIR/usr/local/bin/meshchatsync
+if [[ "$OSNAME" == "debian" ]]; then
+    install -d $IPK_DIR/DEBIAN
+    install -m 644 package/meshchat/* $IPK_DIR/DEBIAN
+    install -d $IPK_DIR/var/www/html/meshchat
+    install www/* $IPK_DIR/var/www/html/meshchat
+    install -d $IPK_DIR/usr/lib/cgi-bin
+    install -m 755 meshchat $IPK_DIR/usr/lib/cgi-bin
+    install -m 644 meshchatlib.lua $IPK_DIR/usr/lib/cgi-bin
+    install -m 644 meshchatconfig.lua $IPK_DIR/usr/lib/cgi-bin
+    install -D support/meshchatsync-init.d -m 755 $IPK_DIR/etc/init.d/meshchatsync
+    install -D support/meshchatsync -m 755 $IPK_DIR/usr/local/bin/meshchatsync
+    install -d $IPK_DIR/usr/local/lib/lua/5.4/net
+    install -m 644 lib/json.lua $IPK_DIR/usr/local/lib/lua/5.4/net
+elif [[ "$OSNAME" == "openwrt" ]]; then
+    install -d $IPK_DIR/www/meshchat
+    install www/* $IPK_DIR/www/meshchat
+    install -d $IPK_DIR/www/cgi-bin
+    install -m 755 meshchat $IPK_DIR/www/cgi-bin
+    install -m 644 meshchatlib.lua $IPK_DIR/www/cgi-bin
+    install -m 644 meshchatconfig.lua $IPK_DIR/www/cgi-bin
+    install -D support/meshchatsync-init.d -m 755 $IPK_DIR/etc/init.d/meshchatsync
+    install -D support/meshchatsync -m 755 $IPK_DIR/usr/local/bin/meshchatsync
+    install -d $IPK_DIR/usr/lib/lua
+    install -m 644 lib/json.lua $IPK_DIR/usr/lib/lua
+fi
